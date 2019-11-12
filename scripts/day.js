9dc951d0d54a156604146d082f6374fc4fdb77be
VasilyMikhailovichDoestovski <asamoahojugbanajeffrey@gmail.com>
2019-11-12 19:21:02 +0100

    Today's date

---
+++ scripts/day.js
@@ -0,0 +1,4 @@
+const today = new Date();
+const day = today.getDay();
+const daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+alert(`Today is : ${daylist[day]}.`);
